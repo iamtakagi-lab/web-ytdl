af97c3a5855c4c8c31dd076f7bffc6e01cb0efbf
takagi <iam.takagi.dev@gmail.com>
2022-02-13 22:19:09 +0900

    Fix: url (web-ytdl.iamtakagi.net)

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 const urlInput = document.getElementById('url')
 const downloadButton = document.getElementById('download')
 downloadButton.addEventListener('click', () => {
-  window.open(`http://localhost:8080/download?url=${urlInput.value}`)
+  window.open(`https://web-ytdl.iamtakagi.net/download?url=${urlInput.value}`)
 })
