902ec4a88ba0385455e6ae33b3538f7a6c3b3c28
takagi <iam.takagi.dev@gmail.com>
2022-02-13 22:16:58 +0900

    Add: webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const nodeExternals = require("webpack-node-externals");
+const isProduction = process.env.NODE_ENV === "production"
+
+/** @type {import("webpack").Configuration} */
+const config = {
+    mode: isProduction ? "production" : "development",
+    entry: {
+        main: "./src/index.ts",
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                options: { compilerOptions: { module: "es2020", moduleResolution: "node" } },
+            },
+            {
+                test: /\.tsx$/,
+                loader: "ts-loader",
+                options: { compilerOptions: { module: "es2020", moduleResolution: "node" } },
+            }
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    devtool: "source-map"
+}
+
+module.exports = config
