eb734605126be8f3649fb1ade98b638881fed331
takagi <iam.takagi.dev@gmail.com>
2022-02-16 15:43:17 +0900

    [serve] Fix: path.resolve

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ router.get("/download", async (ctx, next) => {
   fs.unlinkSync(destFilePath)
 });
 
-app.use(serve(path.join(__dirname, '..', 'public')));
+app.use(serve(path.resolve(__dirname, '..', 'public')));
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.listen(process.env.PORT || 8080);
